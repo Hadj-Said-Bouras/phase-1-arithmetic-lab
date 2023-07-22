fdb4014f9784b2530d915e7e1d58a51959f920dc
Hadj Said Bouras <hadjsaidbouras@hotmail.com>
2023-07-22 17:25:54 +0100

    Done with the assignment

--- index.js
+++ index.js
@@ -1,2 +1,17 @@
+const num1 = 31;
+const num2 = 2;
+const multiply = num1 * num2;
 
-// Write your code here
+console.log(multiply)
+
+const random = Math.floor(Math.random() * 10) + 1;
+console.log(random)
+
+const num3 = 16
+const num4 = 4
+const mod = num4 % num3
+
+console.log(mod)
+
+const max = Math.max(5, 6, 7, 19, 20)
+console.log(max)
